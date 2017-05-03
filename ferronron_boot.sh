4a21e028f4f2cfacaa836c6dc89f3c4a0766b71f
pessonnier <loic.pessonnier@gmail.com>
2017-05-03 23:18:20 +0200

    Update and rename ferronronbox_boot.sh to ferronron_boot.sh

---
+++ ferronron_boot.sh
@@ -0,0 +1,7 @@
+# mettre à jour les scriptes
+cd /home/ferronron
+git clone https://github.com/pessonnier/ferronron_core.git
+git clone https://github.com/pessonnier/ferronron_squelette.git
+
+# les executer
+/home/ferronron/ferronron_core/scripts/frr_start.sh
--- ferronronbox_boot.sh
+++
@@ -1,6 +0,0 @@
-# mettre à jour les scriptes
-cd /home/ferronron
-git clone XXX
-
-# les executer
-/home/ferronron/frr_start.sh
