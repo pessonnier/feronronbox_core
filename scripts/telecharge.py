98cc2d22a5d3f93a7af47b659be7429e7362af63
pessonnier <loic.pessonnier@gmail.com>
2017-05-07 00:11:34 +0200

    Update telecharge.py

--- scripts/telecharge.py
+++ scripts/telecharge.py
@@ -1,1 +1,8 @@
-# rien
+import csv
+import youtube_dl
+
+with open('./ferronron_squelette/liste_youtube.csv','r') as y:
+  c = csv.reader(y,delimiter=',')
+  for l in c:
+    with youtube_dl.YoutubeDL({'outtmpl':l[0],'format':'best[height<=240]'}) as ydl
+      ydl.download(['http://www.youtube.com/watch?v='+l[1][1:]])
