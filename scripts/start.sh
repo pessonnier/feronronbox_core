787bbdd4a4c6f6b4c488422858760c62f27a2ff7
pessonnier <loic.pessonnier@gmail.com>
2017-05-07 16:19:09 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 echo alu
 
-$FRR_HOME/ferronron_core/scripts/init.sh
+. $FRR_HOME/ferronron_core/scripts/init.sh
 python3 $FRR_HOME/ferronron_core/scripts/interrupteurs.py &
 python3 $FRR_HOME/ferronron_core/scripts/play.py &
-$FRR_HOME/ferronron_core/scripts/supprime.sh
+. $FRR_HOME/ferronron_core/scripts/supprime.sh
 python3 $FRR_HOME/ferronron_core/scripts/telecharge.py &
