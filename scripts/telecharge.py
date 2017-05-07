9d5bd216cf319df68c4910d719b3be8b5816b7d8
pessonnier <loic.pessonnier@gmail.com>
2017-05-07 16:41:54 +0200

    Update telecharge.py

--- scripts/telecharge.py
+++ scripts/telecharge.py
@@ -7,6 +7,6 @@ import os
 path=os.environ["FRR_HOME"]+'/'+os.environ["FRR_CONF"]
 with open(path+'/liste_youtube.csv','r') as y:
   c = csv.reader(y,delimiter=',')
-  for l in c[1:]:
+  for l in c:
     with youtube_dl.YoutubeDL({'outtmpl':path+'/media/'+l[0],'format':'best[height<=240]'}) as ydl:
       if l[1]!=' id': ydl.download(['http://www.youtube.com/watch?v='+l[1]])
