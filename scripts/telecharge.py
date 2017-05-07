de980c2489ad93ee5fd691c531427f573173d15b
pessonnier <loic.pessonnier@gmail.com>
2017-05-07 16:41:06 +0200

    Update telecharge.py

--- scripts/telecharge.py
+++ scripts/telecharge.py
@@ -1,9 +1,12 @@
+print("téléchargement youtube")
+
 import csv
 import youtube_dl
+import os
 
-print("téléchargement youtube")
-with open('/home/pi/ferronron_squelette/liste_youtube.csv','r') as y:
+path=os.environ["FRR_HOME"]+'/'+os.environ["FRR_CONF"]
+with open(path+'/liste_youtube.csv','r') as y:
   c = csv.reader(y,delimiter=',')
   for l in c[1:]:
-    with youtube_dl.YoutubeDL({'outtmpl':"/home/pi/ferronron_squelette/media/"+l[0],'format':'best[height<=240]'}) as ydl:
-      ydl.download(['http://www.youtube.com/watch?v='+l[1]])
+    with youtube_dl.YoutubeDL({'outtmpl':path+'/media/'+l[0],'format':'best[height<=240]'}) as ydl:
+      if l[1]!=' id': ydl.download(['http://www.youtube.com/watch?v='+l[1]])
