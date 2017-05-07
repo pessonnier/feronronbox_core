d88f99cfe5f2df4020721c624469aec5fced1aa1
pessonnier <loic.pessonnier@gmail.com>
2017-05-07 21:52:44 +0000

    add play

--- scripts/play.py
+++ scripts/play.py
@@ -1,2 +1,18 @@
 # rien
 print("lecture des videos")
+
+import csv
+import subprocess
+import os
+
+os.putenv('SDL_FBDEV', '/dev/fb1')
+os.putenv('SDL_VIDEODRIVER', 'fbcon')
+os.putenv('SDL_NOMOUSE', '1')
+
+path=os.environ["FRR_HOME"]+'/'+os.environ["FRR_CONF"]
+with open(path+'/playliste1.csv','r') as play:
+  c = csv.reader(play,delimiter=',')
+  for l in c:
+    if l[0]!='nom du fichier': 
+      p=subprocess.Popen(['mplayer', '-vo', 'sdl', path+'/media/'+l[0]])
+      p.wait()
