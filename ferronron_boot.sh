baf3d3fa60fd2f4fe83f51b10c8a8df7dd270c36
pessonnier <loic.pessonnier@gmail.com>
2017-05-07 15:24:40 +0200

    Update ferronron_boot.sh

--- ferronron_boot.sh
+++ ferronron_boot.sh
@@ -2,6 +2,10 @@
 cd /home/pi
 git clone https://github.com/pessonnier/ferronron_core.git
 git clone https://github.com/pessonnier/ferronron_squelette.git
+cd /home/pi/ferronron_core
+git pull
+cd /home/pi/ferronron_squelette
+git pull
 
 # les executer
 ./ferronron_core/scripts/start.sh
