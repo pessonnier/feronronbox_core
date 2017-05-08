342850a4112704e8b81338f015ca6149d6b82770
pessonnier <loic.pessonnier@gmail.com>
2017-05-08 21:36:24 +0000

    maj

--- scripts/play.py
+++ scripts/play.py
@@ -4,6 +4,7 @@ print("lecture des videos")
 import csv
 import subprocess
 import os
+import time
 
 os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_VIDEODRIVER', 'fbcon')
@@ -14,5 +15,8 @@ with open(path+'/playliste1.csv','r') as play:
   c = csv.reader(play,delimiter=',')
   for l in c:
     if l[0]!='nom du fichier': 
-      p=subprocess.Popen(['mplayer', '-vo', 'sdl', path+'/media/'+l[0]])
+      p=subprocess.Popen(['mplayer', '-slave', '-quiet', '-vo', 'sdl', path+'/media/'+l[0]],stdin=subprocess.PIPE)
+      time.sleep(5)
+      print('cmd quit\n')
+      p.stdin.write(b"quit\n")
       p.wait()
