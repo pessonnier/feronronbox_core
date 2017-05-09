f520e74b62fd7e0da928936d7047e9c54e926cb8
pessonnier <loic.pessonnier@gmail.com>
2017-05-09 22:25:12 +0200

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,2 +1,12 @@
 # rien
 echo "initialisation"
+ping -c 1 google.com
+if [ $? ]
+then
+  echo "réseau accessible"
+else
+  echo "réseau inaccessible"
+  cp $FRR_HOME/$FRR_CONF/wpa /etc/wpa_supplicant/wpa_supplicant.con
+  sudo wpa_cli reconfigure
+  sleep 3
+fi
