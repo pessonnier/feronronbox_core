8fc1e00c6bf630b25fa18b93418e275d4cfa7abb
pessonnier <loic.pessonnier@wanadoo.fr>
2017-05-14 20:36:55 +0000

    clone

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,15 +1,15 @@
-# rien
-echo "****************"
-date
-echo "****************"
-echo "initialisation"
-ping -c 1 google.com
-if [ $? ]
-then
-  echo "réseau accessible"
-else
-  echo "réseau inaccessible, maj de la conf"
-  cp $FRR_HOME/$FRR_CONF/wpa /etc/wpa_supplicant/wpa_supplicant.con
-  sudo wpa_cli reconfigure
-  sleep 3
-fi
+while true
+do
+  echo "initialisation"
+  ping -c 1 google.com
+  if [ $? ]
+  then
+    echo "réseau accessible"
+    break 
+  else
+    echo "réseau inaccessible, maj de la conf"
+    cp $FRR_HOME/$FRR_CONF/wpa /etc/wpa_supplicant/wpa_supplicant.con
+    sudo wpa_cli reconfigure
+    sleep 3
+  fi
+done
--- scripts/miseajour.sh
+++ scripts/miseajour.sh
@@ -1,10 +1,19 @@
 echo "mise à jour"
 echo $FRR_HOME
 cd $FRR_HOME
-git clone https://github.com/pessonnier/ferronron_core.git
-git clone https://github.com/pessonnier/$FRR_CONF.git
+echo "clone"
+if [ ! -d "ferronron_core" ]
+then
+  git clone https://github.com/pessonnier/ferronron_core.git
+fi
+if [ ! -d $FRR_CONF ]
+then
+  git clone https://github.com/pessonnier/$FRR_CONF.git
+fi
+echo "pull"
 cd $FRR_HOME/ferronron_core
 git pull
 cd $FRR_HOME/$FRR_CONF
 git pull
 cd $FRR_HOME
+echo "fin init"
