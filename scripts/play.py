271842d1c26bdd6202695fb265020bc679977262
pessonnier <loic.pessonnier@gmail.com>
2017-05-14 12:11:26 +0200

    Update play.py

--- scripts/play.py
+++ scripts/play.py
@@ -10,14 +10,23 @@ os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_VIDEODRIVER', 'fbcon')
 os.putenv('SDL_NOMOUSE', '1')
 
+def mpcmd(p,cmd):
+  print(cmd+'\n')
+  p.stdin.write(cmd+'\n')
+  p.stdin.flush()
+
 path=os.environ["FRR_HOME"]+'/'+os.environ["FRR_CONF"]
+p=subprocess.Popen(['mplayer', '-slave', '-quiet', '-vo', 'sdl'],stdin=subprocess.PIPE)
 with open(path+'/playliste1.csv','r') as play:
   c = csv.reader(play,delimiter=',')
   for l in c:
     if l[0]!='nom du fichier': 
-      p=subprocess.Popen(['mplayer', '-slave', '-quiet', '-vo', 'sdl', path+'/media/'+l[0]],stdin=subprocess.PIPE)
+      mpcmd(p,b'loading '+ path+'/media/'+l[0])
       time.sleep(15)
-      print('cmd quit\n')
-      p.stdin.write(b"quit\n")
-      p.stdin.flush()
-      p.wait()
+      mpcmd(p,b'pause')
+      time.sleep(1)      
+      mpcmd(p,b'play')
+      #print('cmd quit\n')
+      #p.stdin.write(b"quit\n")
+      #p.stdin.flush()
+      #p.wait()
