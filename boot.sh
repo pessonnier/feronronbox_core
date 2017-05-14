d2ee579783fa85330037665f75103e59a81578d6
pessonnier <loic.pessonnier@wanadoo.fr>
2017-05-14 22:43:23 +0000

    boot

---
+++ boot.sh
@@ -0,0 +1,9 @@
+# mettre à jour les scriptes
+# conf par défaut à supprimer et à personnaliser dans le bashrc par exemple
+export FRR_HOME=/home/pi
+export FRR_CONF=ferronron_squelette 
+
+sudo fbi -T 2 -d /dev/fb1 -noverbose -a $FRR_HOME/ferronron_core/acceuil.jpg
+
+# les executer
+. $FRR_HOME/ferronron_core/scripts/start.sh &>> $FRR_HOME/$FRR_CONF/log/script.log
