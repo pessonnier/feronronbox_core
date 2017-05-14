94c1b8f965dbe000bb511a585df4fe16edb1fed3
pessonnier <loic.pessonnier@wanadoo.fr>
2017-05-14 22:13:25 +0000

    sudo -E

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,8 @@ date
 echo "***************"
 . $FRR_HOME/ferronron_core/scripts/miseajour.sh
 . $FRR_HOME/ferronron_core/scripts/init.sh
-sudo python3 $FRR_HOME/ferronron_core/scripts/interrupteurs.py &
-sudo python3 $FRR_HOME/ferronron_core/scripts/play.py &
+sudo -E python3 $FRR_HOME/ferronron_core/scripts/interrupteurs.py &
+sudo -E python3 $FRR_HOME/ferronron_core/scripts/play.py &
 . $FRR_HOME/ferronron_core/scripts/supprime.sh
 . $FRR_HOME/ferronron_core/scripts/upload.sh
 python3 $FRR_HOME/ferronron_core/scripts/telecharge.py &
